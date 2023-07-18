22fc292423ebf04ead1da8e78e31ee9dabcaf3fb
Chirag00509 <chiraglabha31@gmail.com>
2023-07-18 18:29:32 +0530

    add data

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Modal;
+using System.Security.Cryptography;
 
 namespace WebApplication1.Controllers
 {
@@ -61,7 +63,7 @@ namespace WebApplication1.Controllers
 
         // POST: api/User
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
+        [HttpPost("/api/register")]
         public async Task<ActionResult<User>> PostUser(User user)
         {
             var email  = _context.User.Any(u => u.Email == user.Email);
@@ -76,10 +78,33 @@ namespace WebApplication1.Controllers
                 return BadRequest(new { message = "Registration failed due to validation errors." });
             }
 
+            var convertPassword = hashPassword(user.Password);
+
+            user.Password = convertPassword;
             _context.User.Add(user);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        [HttpPost("/api/login")]
+
+        public async Task<ActionResult<User>> login(string email, string password) 
+        {
+            var convertPassword = hashPassword(password);
+            var users = await _context.User.FirstOrDefaultAsync(u=> u.Email == email &&  u.Password == convertPassword ); 
+
+            if (users == null)
+            {
+                return Unauthorized(new { message = "Login failed due to incorrect credentials" });
+            }
+
+            if(!ModelState.IsValid) 
+            {
+                return BadRequest(new { message = "Login failed due to validation errors." });
+            }
+
+            return Ok(new { message = "Successfull" });
         }
 
         // DELETE: api/User/5
@@ -101,6 +126,17 @@ namespace WebApplication1.Controllers
         private bool UserExists(int id)
         {
             return _context.User.Any(e => e.Id == id);
+        }
+
+        private string hashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+
         }
     }
 }
